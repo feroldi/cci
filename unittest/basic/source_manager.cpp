ef3e34a23bfdac8f606916ebc5862051321023a1
Mário Feroldi <mferoldif@gmail.com>
2017-11-10 17:22:24 -0200

    Add SourceLocation unit tests

--- unittest/basic/source_manager.cpp
+++ unittest/basic/source_manager.cpp
@@ -3,6 +3,15 @@
 
 namespace {
 
+TEST(BasicTest, compareSourceLocations)
+{
+  cci::SourceLocation l1(0U);
+  auto l2 = l1.based_on(cci::SourceLocation::with_offset(1U));
+
+  EXPECT_GT(l2, l1);
+  EXPECT_EQ(cci::SourceLocation(2U), cci::SourceLocation::with_offset(2U));
+}
+
 TEST(SourceManagerTest, calcLineOffsets)
 {
   EXPECT_TRUE(true);
