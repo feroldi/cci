3051141913ff49c09ab523d1d377362b5db693a0
Guillaume Papin <guillaume.papin@epitech.eu>
2017-12-14 23:28:32 +0100

    make git_revision.cpp self-contained

--- src/cci/cci.cpp
+++ src/cci/cci.cpp
@@ -1,15 +1,9 @@
 #include <fmt/format.h>
 #include <string>
 
-#ifdef CCI_USING_GIT_REVISION
 auto cci_git_refspec() -> const char *;
 auto cci_git_hash() -> const char *;
 auto cci_git_tag() -> const char *;
-#else
-auto cci_git_refspec() -> const char * { return ""; }
-auto cci_git_hash() -> const char * { return ""; }
-auto cci_git_tag() -> const char * { return ""; }
-#endif
 
 static auto cci_get_build_version() -> std::string
 {
