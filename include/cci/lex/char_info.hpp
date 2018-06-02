66822e616c1c3973a24950c88e26edf3fdaca414
Mário Feroldi <mferoldif@gmail.com>
2018-06-02 19:33:41 -0300

    Fix is_printable returning true for >127 values

--- include/cci/lex/char_info.hpp
+++ include/cci/lex/char_info.hpp
@@ -8,11 +8,10 @@ extern const uint8_t ASCIITable[256];
 enum : uint8_t
 {
   CHAR_ALPHA = 0x1, // Alphabetic
-  CHAR_CONTROL = 0x2, // Control character
-  CHAR_DIGIT = 0x4, // Numeric digit
-  CHAR_OCTDIG = 0x8, // Octal numeric digit
-  CHAR_HEXDIG = 0x10, // Hexadecimal numeric digit
-  CHAR_PUNCT = 0x20, // Punctuation
+  CHAR_DIGIT = 0x2, // Numeric digit
+  CHAR_OCTDIG = 0x4, // Octal numeric digit
+  CHAR_HEXDIG = 0x8, // Hexadecimal numeric digit
+  CHAR_PUNCT = 0x10, // Punctuation
 };
 } // namespace charinfo
 
@@ -34,12 +33,6 @@ inline bool is_octdigit(unsigned char c)
   return ASCIITable[c] & CHAR_OCTDIG;
 }
 
-inline bool is_control(unsigned char c)
-{
-  using namespace charinfo;
-  return ASCIITable[c] & CHAR_CONTROL;
-}
-
 inline bool is_alpha(unsigned char c)
 {
   using namespace charinfo;
@@ -52,7 +45,11 @@ inline bool is_alphanum(unsigned char c)
   return ASCIITable[c] & (CHAR_ALPHA | CHAR_DIGIT);
 }
 
-inline bool is_printable(unsigned char c) { return !is_control(c); }
+inline bool is_printable(unsigned char c)
+{
+  using namespace charinfo;
+  return ASCIITable[c] & (CHAR_ALPHA | CHAR_DIGIT | CHAR_PUNCT);
+}
 
 constexpr inline bool is_ascii(char c)
 {
