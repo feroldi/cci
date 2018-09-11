479eb0458c28e4e0f7976c88990f31fd8c9811d7
Mário Feroldi <mferoldif@gmail.com>
2018-09-11 15:28:48 -0300

    Remove GitRevision CMake module

--- src/cci/cci.cpp
+++ src/cci/cci.cpp
@@ -1,18 +1,1 @@
-#include <fmt/format.h>
-#include <string>
-
-auto cci_git_refspec() -> const char *;
-auto cci_git_hash() -> const char *;
-auto cci_git_tag() -> const char *;
-
-static auto cci_get_build_version() -> std::string
-{
-  return fmt::format("{} {} {}", cci_git_refspec(), cci_git_tag(),
-                     cci_git_hash());
-}
-
-int main()
-{
-  fmt::print(stderr, "{}\n", cci_get_build_version());
-  return 0;
-}
+int main() { }
